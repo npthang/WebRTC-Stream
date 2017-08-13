069b96c6820f043176d4e959b573005a7c9021f5
npthang <npthang02959@gmail.com>
2017-08-13 19:29:04 +0700

    deploy

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ app.use(express.static("public"));
 
 app.get('/', (req, res) => res.render('home'));
 
-app.listen(3000, () => console.log('Server started'));
+app.listen(process.env.PORT || 3000, () => console.log('Server started'));
